e97b141c1ab4850bcab4ecd91778ad1869d075a1
pranavshukla1011 <shuklapranav1011@gmail.com>
2021-03-15 11:05:02 +0530

    button added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 const github = new Github;
 const ui = new UI;
-const searhUser = document.querySelector('#username');
-searhUser.addEventListener('keyup', (e)=>{
-    const userText = e.target.value;
+const searhUser = document.querySelector('#button');
+const userName = document.querySelector('#username')
+searhUser.addEventListener('click', (e)=>{
+    const userText = userName.value;
     if(userText !== ''){
-       
 
         github.getUser(userText)
         .then((data) => {
@@ -22,4 +22,6 @@ searhUser.addEventListener('keyup', (e)=>{
         //clear profile
        ui.clearProfile();
     }
+
+    e.preventDefault();
 })
